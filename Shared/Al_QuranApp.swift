e497ab5ef8a64fbaf22bb3f6ae3df7ca277c64f7
Asim Khan <iamasimkhanjadoon@gmail.com>
2021-09-25 10:54:39 +0400

    Initial Commit

---
+++ Shared/Al_QuranApp.swift
@@ -0,0 +1,17 @@
+//
+//  Al_QuranApp.swift
+//  Shared
+//
+//  Created by Asim Khan on 25/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct Al_QuranApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
